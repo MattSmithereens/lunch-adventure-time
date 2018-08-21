8957e391942942efb519d8e730b138f12c161e72
Matt Smith and Ryan Murry <matt+ryan@>
2018-08-21 11:12:19 -0700

    add choice route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ChoiceComponent } from './choice/choice.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'choice',
+    component: ChoiceComponent
   }
 ];
 
