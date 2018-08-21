ba727336c526043392316e4e5235a3c3e62b536c
Matt Smith and Ryan Murry <matt+ryan@>
2018-08-21 11:09:05 -0700

    create new components

---
+++ src/app/mexican-food/mexican-food.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MexicanFoodComponent } from './mexican-food.component';
+
+describe('MexicanFoodComponent', () => {
+  let component: MexicanFoodComponent;
+  let fixture: ComponentFixture<MexicanFoodComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MexicanFoodComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MexicanFoodComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
