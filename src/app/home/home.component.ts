daeb9c240c2c0e593ef6247205d611c3da18ff4c
Matt Smith and Ryan Murry <matt+ryan@>
2018-08-21 11:34:40 -0700

    import player service and update function to save player to database

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { Player } from '../models/player';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [PlayerService]
 })
 export class HomeComponent {
 
+  constructor(private playerService: PlayerService) {
+
+  }
+
   submitForm(name: string, lunch: string, color: string) {
     let newPlayer: Player = new Player(name, lunch, color);
-    console.log(newPlayer);
+    this.playerService.addPlayer(newPlayer);
   }
 }
